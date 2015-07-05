f7daf959638675b8308171ad92ed356cdb8efd69
August Guang <august.guang@gmail.com>
2015-07-05 13:05:39 -0400

    modified phase1.py to use absolute pathname for isg so that calling from a different directory will not affect it

--- phase1.py
+++ phase1.py
@@ -2,62 +2,55 @@
 import os
 import sys, string
 import random as rand
-import dendropy
+from ete2 import Tree
 
 # simulate sequences from specified gene tree
-def simulate_sequences(species_tree, root_file, species_tree_file, num_genefams,out_dir):
-    print "Step 2: Sequence Evolution Based on Species Tree"
-
+def simulate_sequences(tree, root_file, tree_file, out_dir):
     """Simulate sequences from set of gene trees using iSG. Rates of evolution drawn from random distributions."""
-    species_tree.is_rooted=True
     # write tree_file in [:root_file] "Label" (tree) format
-    for i in range(num_genefams):
-        infile = out_dir + species_tree_file + str(i+1)
-        with open(infile, 'w') as fout:
-            fout.write('[:' + root_file + str(i+1) + '] ')
-            fout.write('" Label ' + '" ')
-            s = species_tree.as_string('newick')
-            s = s[5:] # get rid of rooting
-            last_parens = s.rfind(')')
-            fout.write(s[:last_parens+1])
-            fout.write(';\n')
-            fout.close()
-        outfile = infile + '_sequences'
-        paramfile = infile + '_params'
+    infile = out_dir
+    with open(infile, 'w') as fout:
+        fout.write('[:' + root_file + '] ')
+        fout.write('" Label ' + '" ')
+        fout.write("{10, 0}")
+        s=tree.write(format=5)
+        fout.write(s)
+        fout.write('\n')
+        fout.close()
+    outfile = infile + '_sequences'
+    paramfile = infile + '_params'
 
-        # set up iSG simulation parameters
-        options = ''
-        matrix = ''
-        # rate substitution parameters
-        # -m (string) HKY, F84 for substitution matrix
-        # -a (float) gamma rate heterogeneity OR
-        # -c (list) codon position heterogeneity OR
-        # -g (int) numbe categories for discrete gamma-dist rate heterogeneity (b/w 2 and 32)
-        rate_type = rand.randint(0, 2)
-        options = {
-            0: '-a ' + str(rand.uniform(0.01, 2)),
-            1: '-c ' + ','.join([str(rand.uniform(0.1,0.5)),str(rand.uniform(0.01,0.1)),str(rand.uniform(0.5,0.99))]),
-            2: '-a ' + str(rand.uniform(0.01, 2)) + ' -g ' + str(rand.randint(2,32))
-            }[rate_type]
-        matrix = {
-            0: 'HKY',
-            1: 'F84'
-            }[rand.randint(0,1)]
-        with open(paramfile, 'a') as p:
-            p.write("codon\n")
-            p.write("rate type: " + options + "\n")
-            p.write("matrix: " + matrix + "\n")
+    # set up iSG simulation parameters
+    options = ''
+    matrix = ''
+    # rate substitution parameters
+    # -m (string) HKY for substitution matrix
+    # -a (float) gamma rate heterogeneity OR
+    # -c (list) codon position heterogeneity OR
+    # -g (int) number categories for discrete gamma-dist rate heterogeneity (b/w 2 and 32)
+    rate_type = rand.randint(0, 2)
+    options = {
+        0: '-a ' + str(rand.uniform(0.01, 2)),
+        1: '-c ' + ','.join([str(rand.uniform(0.1,0.5)),str(rand.uniform(0.01,0.1)),str(rand.uniform(0.5,0.99))]),
+        2: '-a ' + str(rand.uniform(0.01, 2)) + ' -g ' + str(rand.randint(2,32))
+        }[rate_type]
+    matrix = 'HKY'
+    with open(paramfile, 'a') as p:
+        p.write("codon\n")
+        p.write("rate type: " + options + "\n")
+        p.write("matrix: " + matrix + "\n")
+        
+    # invariance parameter
+    # -i (float) proportion of invariable sites
+    inv_rate = '-i ' + str(rand.random())
+    with open(paramfile, 'a') as p:
+        p.write("invariance rate: " + inv_rate + "\n")
+        p.write("---------------\n")
 
-        # invariance parameter
-        # -i (float) proportion of invariable sites
-        inv_rate = '-i ' + str(rand.random())
-        with open(paramfile, 'a') as p:
-            p.write("invariance rate: " + inv_rate + "\n")
-            p.write("---------------\n")
-
-        ret = os.system('./indel-seq-gen -m %s %s --outfile %s < %s' % (matrix, options, outfile, infile))
-        if ret != 0:
-            raise OSError, 'indel-seq-gen failed with code %s ' % ret
+    path = os.path.dirname(sys.argv[0])
+    path = os.path.abspath(path)
+    ret = os.system('%s/indel-seq-gen -m %s %s --outfile %s < %s' % (path, matrix, options, outfile, infile))
+#            raise OSError, 'indel-seq-gen failed with code %s ' % ret
 
 def write_root(root_file, num_genefams):
     """Write root sequences in iSG format by pulling from given file with root sequences"""
@@ -72,15 +65,15 @@ def write_root(root_file, num_genefams):
             seq = line[1].upper()
             l = len(seq) - 1
 #            if l > 200: # only take amino acid sequences with at least 200
-#            if l > 600: # only take sequences with at least 600
-            out = root_file + str(count)
-            with open(out, 'w') as fout:
-                fout.write(str(l))
-                fout.write('\n')
-                fout.write('0'*l+'\n')
-                fout.write(seq)
-                fout.close()
-            count = count + 1
+            if l > 600: # only take sequences with at least 600
+                out = root_file + str(count)
+                with open(out, 'w') as fout:
+                    fout.write(str(l))
+                    fout.write('\n')
+                    fout.write('0'*l+'\n')
+                    fout.write(seq)
+                    fout.close()
+                count = count + 1
 
 if __name__ == "__main__":
 
@@ -99,7 +92,7 @@ if __name__ == "__main__":
     opts = parser.parse_args()
 
     species_tree_file = opts.species_tree
-    species_tree = dendropy.Tree.get_from_path(species_tree_file, schema="newick", rooted=True)
+    species_tree = Tree(species_tree_file)
     num_genefams = int(opts.num_genefams)
     root_seq = opts.root_seq
     wr_flag = int(opts.wr_flag)
