b9b2076789d0488e8588407b25ed8bef5e65835c
August Guang <august.guang@gmail.com>
2017-11-07 16:43:37 -0500

    yaml params file

--- transmissim/simulate.py
+++ transmissim/simulate.py
@@ -44,9 +44,6 @@ def outbreaker(cluster_R0, n_hosts, cluster_duration, rate_import_case):
     return(test)
 
 def transmission_tree(network, cluster_duration, ancestral_duration, out, simphy_path, seed, birth_rate, death_rate):
-    ances = [i for i in network[4]]
-    onset = [i for i in network[2]]
-    vt = viral(onset, cluster_duration, ancestral_duration, ances, birth_rate, death_rate, seed, simphy_path, out)
     full_trees = binary_trees(network)
     i = 0
     with open('%s/clusters.txt' % (out), 'w') as g:
@@ -59,9 +56,14 @@ def transmission_tree(network, cluster_duration, ancestral_duration, out, simphy
             with open('%s/simulated_tree_%s.tre' % (out, i), 'w') as f:
                f.write(tree)
             i = i+1
+    return(full_trees)
+
+def viral_tree(network, cluster_duration, ancestral_duration, ances, birth_rate, death_rate, seed, simphy_path, out):
+    ances = [i for i in network[4]]
+    onset = [i for i in network[2]]
+    vt = viral(onset, cluster_duration, ancestral_duration, ances, birth_rate, death_rate, seed, simphy_path, out)
     with open('%s/simulated_viral.tre' % (out), 'w') as f:
         f.write(vt.write(format=5))
-
     return vt
 
 def reads(art, sequencing_system, reads_out, read_length, coverage):
@@ -79,77 +81,3 @@ def reads(art, sequencing_system, reads_out, read_length, coverage):
                 SeqIO.write(record, f, "fasta")
                 f.write(">%s'\n" % record.id)
                 f.write(str(record.seq.reverse_complement())+"\n")
-
-if __name__ == "__main__":
-
-    import argparse
-    parser = argparse.ArgumentParser(description='simulation')
-
-    ds = ' [%(default)s]'
-    parser.add_argument('-p', '--params', help='parameters file for simulation')
-    opts = parser.parse_args()
-
-    params = opts.params
-    if params:
-        options = [line.strip().split('//')[0].strip() for line in open(params)]
-        
-        analysis_start = options[0]
-        print(analysis_start)
-        analysis_end = options[1]
-        seed = options[2]
-        if not seed:
-            seed = random.randint(1,4294967295)
-        else: seed = int(seed)
-        # transmission tree options
-        cluster_R0 = float(options[7])
-        cluster_death = float(options[8])
-        n_hosts = int(options[9])
-        cluster_duration = int(options[10])
-        rate_import_case = float(options[11])
-
-        ancestral_R0 = float(options[12])
-        ancestral_death = float(options[13])
-        ancestral_duration = float(options[14])
-
-        tree_out = options[15]
-
-        # viral tree options
-        simphy_path = options[18]
-        birth_rate = options[19]
-        death_rate = options[20]
-
-        # pyvolve options
-        full_tree = options[21]
-        root_file = options[25]
-
-        # ART options
-        art = options[28]
-        reads_out = options[29]
-        sequencing_system = options[30]
-        read_length = options[31]
-        coverage = options[32]
-        mean_fragment_length = options[33]
-        sd_fragment_length = options[34]
-
-        print(analysis_start == "Pipeline: all")
-        if analysis_start == "all":
-            network = outbreaker(cluster_R0, n_hosts, cluster_duration, rate_import_case)
-            vt = transmission_tree(network, cluster_duration, ancestral_duration, tree_out, simphy_path, seed, birth_rate, death_rate)
-            if analysis_end != "TT":
-                t = vt.write(format=5)
-                sequence(t, root_file)
-                print("sequence finished")
-                if analysis_end != "GS":
-                    reads(art, sequencing_system,reads_out,read_length,coverage,mean_fragment_length,sd_fragment_length)
-
-        if analysis_start == "TT":
-            print("Pipeline: Transmission Tree -> Reads")
-            t=''
-            with open(full_tree, 'r') as f:
-                t=f.readline()
-            sequence(t, root_file)
-            if analysis_end != "GS":
-                reads(art, sequencing_system, reads_out, read_length, coverage,mean_fragment_length,sd_fragment_length)
-
-        if analysis_start == "GS":
-            reads(art, sequencing_system, reads_out, read_length, coverage,mean_fragment_length,sd_fragment_length)
